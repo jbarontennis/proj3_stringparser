9ffc870ca3b540c0fe6689063857a686084f8cac
James Baron <jbarontennis@gmail.com>
2019-11-01 23:41:57 -0400

    initial

--- StringParserClass.cpp
+++ StringParserClass.cpp
@@ -7,6 +7,37 @@
 
 #include <string>
 #include <string.h>
+#include "../327_proj3_test/includes/StringParserClass.h"
+#include "../327_proj3_test/includes/constants.h"
+#include "stdlib.h"
+ char    *pStartTag;
+        char    *pEndTag;
+        bool    areTagsSet;
+        KP_StringParserClass::StringParserClass::StringParserClass(void) {
+            pStartTag = NULL;
+            pEndTag = NULL;
+            areTagsSet = false;
+        }
 
-//TODO Fill in
+        KP_StringParserClass::StringParserClass::~StringParserClass(void) {
+            cleanup();
+        }
+
+        int KP_StringParserClass::StringParserClass::setTags(const char* pStart, const char* pEnd) {
+
+        }
+
+        int KP_StringParserClass::StringParserClass::getDataBetweenTags(
+                char* pDataToSearchThru, std::vector<std::string>& myVector) {
+
+        }
+
+        void KP_StringParserClass::StringParserClass::cleanup() {
+
+        }
+
+        int KP_StringParserClass::StringParserClass::findTag(char* pTagToLookFor,
+                char*& pStart, char*& pEnd) {
+
+        }
 
